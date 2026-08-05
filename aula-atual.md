e51e8cc2dc8356ca29581eabc49366ad04383a37
maluvicentini <vicentinimalu1@gmail.com>
2026-08-05 09:22:07 -0300

    feat(create-user): rota de criacao de usuarios

--- aula-atual.md
+++ aula-atual.md
@@ -2,6 +2,3 @@ https://www.youtube.com/live/WnhDgVLYfx0?si=JFbg-2jRcGZZbpry
 
 
 parei: 
-nao comecei ainda
-
-essa ai ja e sobre criacao das rotas crud
--- dunossauro_fastapi/app.py
+++ dunossauro_fastapi/app.py
@@ -1,10 +1,17 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
 
-from dunossauro_fastapi.schemas import Message
+from dunossauro_fastapi.schemas import Message, UserSchema, UserPublic
 
 app = FastAPI()
 
 
-@app.get('/', response_model=Message)
+@app.get('/', status_code=HTTPStatus.OK ,response_model=Message)
 def read_root():
     return {'message': 'Hello world!'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    return user 
