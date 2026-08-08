f29293627ab1051a9fdba2e90aff8794d5596ac5
maluvicentini <vicentinimalu1@gmail.com>
2026-08-08 02:24:29 -0300

    test(UD): editing and deleting users tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -19,7 +19,7 @@ def test_create_user_sucess(client):
 
     response = client.post('/users/', json=payload)
     print(vars(response))
-    assert response.status_code == HTTPStatus.CREATED
+    assert response.status_code == HTTPStatus.OK
 
 
 def test_read_users_sucess(client):
@@ -31,3 +31,47 @@ def test_read_users_sucess(client):
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == mockUsers
+
+
+def test_update_user_success(client):
+
+    payload = {
+        'username': 'malu',
+        'email': 'test@malu.com',
+        'password': 'vodoeprajacu',
+    }
+
+    response = client.put('/users/1', json=payload)
+
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_update_user_error(client):
+
+    payload = {
+        'username': 'malu',
+        'email': 'test@malu.com',
+        'password': 'vodoeprajacu',
+    }
+
+    response = client.put('/users/3', json=payload)
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+
+
+def test_delete_user_success(client):
+    message_success = {'message': 'Usuario deletado com sucesso'}
+
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == message_success
+
+
+def test_delete_user_error(client):
+    message_success = {'detail': 'Usuario nao encontrado'}
+
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == message_success
