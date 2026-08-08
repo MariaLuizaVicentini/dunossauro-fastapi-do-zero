06573c69ff488e842c8f45f0818ebb93c0634327
maluvicentini <vicentinimalu1@gmail.com>
2026-08-08 02:23:21 -0300

    feat(PD) implementation of user deletion and editing

--- dunossauro_fastapi/app.py
+++ dunossauro_fastapi/app.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from dunossauro_fastapi.schemas import (
     Message,
@@ -15,12 +15,12 @@ app = FastAPI()
 database = []
 
 
-@app.get('/', response_model=Message)
+@app.get('/', response_model=Message, tags=["Hello World"])
 def read_root():
     return {'message': 'Hello world!'}
 
 
-@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+@app.post('/users/', response_model=UserPublic, tags=["Users"])
 def create_user(user: UserSchema):
     user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
     database.append(user_with_id)
@@ -28,6 +28,33 @@ def create_user(user: UserSchema):
     return user_with_id
 
 
-@app.get('/users/', response_model=UserList)
+@app.get('/users/', response_model=UserList, tags=["Users"])
 def read_users():
     return {'users': database}
+
+
+@app.put('/users/{user_id}', response_model=UserPublic, tags=["Users"])
+def update_user(user_id: int, user: UserSchema):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='Usuario nao encontrado'
+        )
+
+    user_with_id = UserDB(id=len(database), **user.model_dump())
+    print(user_with_id)
+
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete('/users/{user_id}', response_model=Message, tags=["Users"])
+def delete_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='Usuario nao encontrado'
+        )
+
+    del database[user_id - 1]
+
+    return {'message': 'Usuario deletado com sucesso'}
