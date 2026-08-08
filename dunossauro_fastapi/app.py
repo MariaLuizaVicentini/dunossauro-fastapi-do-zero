60ffbcc99d16c1c45d2e8aa1da67389b197da4d4
maluvicentini <vicentinimalu1@gmail.com>
2026-08-08 01:20:12 -0300

    feat(CR): implementation of record creation and reading

--- dunossauro_fastapi/app.py
+++ dunossauro_fastapi/app.py
@@ -2,14 +2,20 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from dunossauro_fastapi.schemas import Message, UserDB, UserPublic, UserSchema
+from dunossauro_fastapi.schemas import (
+    Message,
+    UserDB,
+    UserList,
+    UserPublic,
+    UserSchema,
+)
 
 app = FastAPI()
 
 database = []
 
 
-@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
+@app.get('/', response_model=Message)
 def read_root():
     return {'message': 'Hello world!'}
 
@@ -17,4 +23,11 @@ def read_root():
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema):
     user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
+    database.append(user_with_id)
+
     return user_with_id
+
+
+@app.get('/users/', response_model=UserList)
+def read_users():
+    return {'users': database}
--- tests/test_app.py
+++ tests/test_app.py
@@ -20,3 +20,14 @@ def test_create_user_sucess(client):
     response = client.post('/users/', json=payload)
     print(vars(response))
     assert response.status_code == HTTPStatus.CREATED
+
+
+def test_read_users_sucess(client):
+    mockUsers = {
+        'users': [{'id': 1, 'username': 'malu', 'email': 'test@malu.com'}]
+    }
+
+    response = client.get('users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == mockUsers
