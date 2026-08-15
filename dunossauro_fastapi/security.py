521f460bca6e2a52e8db3966c4be1d43b88b119e
maluvicentini <vicentinimalu1@gmail.com>
2026-08-14 21:23:25 -0300

    style(security): Deixando os comentarios mais explicitos

--- dunossauro_fastapi/security.py
+++ dunossauro_fastapi/security.py
@@ -3,11 +3,18 @@ from pwdlib import PasswordHash
 pwd_context = PasswordHash.recommended()
 
 
-# le a senha e gera o hash da senha
 def get_password_hash(password: str):
+    """
+        Recebe uma senha em texto puro e retorna o hash gerado para ela.
+    """
     return pwd_context.hash(password)
 
 
 # valida se a senha pura bate com o hash do banco de dados
 def verify_password(plain_password: str, hashed_password):
+    """
+        Verifica se a senha em texto puro corresponde ao hash informado
+        True = Corresponde
+        False = Nao corresponde
+    """
     return pwd_context.verify(plain_password, hashed_password)
