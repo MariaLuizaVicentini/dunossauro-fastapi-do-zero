725f6a1c68827743ccb641d1ef386dd363ff36c4
maluvicentini <vicentinimalu1@gmail.com>
2026-08-15 00:00:30 -0300

    test(jwt): verificando se o token contem as informacoes corretas

---
+++ tests/test_security.py
@@ -0,0 +1,13 @@
+from jwt import decode
+
+from dunossauro_fastapi.security import ALGORITHM, SECRET_KEY, create_access_token
+
+
+def test_jwt():
+    data = {'test': 'test'}
+    token = create_access_token(data)
+
+    decoded = decode(token, SECRET_KEY, algorithms=ALGORITHM)
+    breakpoint()
+    assert decoded['test'] == data['test']
+    assert 'exp' in decoded  # testa se o valor de exp foi add ao tokenj
