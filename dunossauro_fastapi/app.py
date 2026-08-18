21a95f6974b9a2cba92bb136d8438440e747da29
malu vicentini <maria@multi360.com.br>
2026-08-18 20:03:54 -0300

    refactor(app.py): centralizando as rotas no arquivo app.py

--- dunossauro_fastapi/app.py
+++ dunossauro_fastapi/app.py
@@ -1,130 +1,16 @@
 from http import HTTPStatus
 
-from fastapi import Depends, FastAPI, HTTPException
-from fastapi.security import OAuth2PasswordRequestForm
-from sqlalchemy import select
-from sqlalchemy.exc import IntegrityError
-from sqlalchemy.orm import Session
+from fastapi import FastAPI
 
-from dunossauro_fastapi.database import get_session
-from dunossauro_fastapi.models import User
-from dunossauro_fastapi.schemas import (
-    Message,
-    Token,
-    UserList,
-    UserPublic,
-    UserSchema,
-)
-from dunossauro_fastapi.security import (
-    create_access_token,
-    get_current_user,
-    get_password_hash,
-    verify_password,
-)
+from dunossauro_fastapi.routers import auth, users
+from dunossauro_fastapi.schemas import Message
 
-app = FastAPI()
+app = FastAPI(title='Peeey!')
+
+app.include_router(auth.router)
+app.include_router(users.router)
 
 
-@app.post('/token', response_model=Token, tags=['Users'])
-def login_for_access_token(
-    form_data: OAuth2PasswordRequestForm = Depends(),
-    session: Session = Depends(get_session),
-):
-    user = session.scalar(select(User).where(User.email == form_data.username))
-
-    if not user:
-        raise HTTPException(
-            status_code=HTTPStatus.UNAUTHORIZED,
-            detail='Email ou senha nao cadastrados no banco',
-        )
-
-    if not verify_password(form_data.password, user.password):
-        raise HTTPException(
-            status_code=HTTPStatus.UNAUTHORIZED, detail='Email ou senha incorretos'
-        )
-
-    access_token = create_access_token(data={'sub': user.email})
-
-    return {'access_token': access_token, 'token_type': 'Bearer'}
-
-
-@app.post('/users/', response_model=UserPublic, tags=['Users'])
-def create_user(user: UserSchema, session: Session = Depends(get_session)):
-
-    user_db = session.scalar(
-        select(User).where((User.email == user.email) | (User.username == user.username))
-    )
-
-    user_db = User(
-        username=user.username,
-        email=user.email,
-        password=get_password_hash(user.password),
-    )
-
-    try:
-        session.add(user_db)
-        session.commit()
-        session.refresh(user_db)
-
-        return user_db
-    except IntegrityError:
-        raise HTTPException(
-            status_code=HTTPStatus.CONFLICT, detail='Username ou email ja existe'
-        )
-
-
-@app.get('/users/', response_model=UserList, tags=['Users'])
-def read_users(
-    limit: int = 10,
-    offset: int = 0,
-    session: Session = Depends(get_session),
-    current_user=Depends(get_current_user),
-):
-    users = session.scalars(select(User).limit(limit).offset(offset))
-    return {'users': users}
-
-
-@app.put('/users/{user_id}', response_model=UserPublic, tags=['Users'])
-def update_user(
-    user_id: int,
-    user: UserSchema,
-    session: Session = Depends(get_session),
-    current_user: User = Depends(get_current_user),
-):
-    if current_user.id != user_id:
-        raise HTTPException(
-            status_code=HTTPStatus.FORBIDDEN, detail='permissões insuficientes'
-        )
-
-    current_user.email = user.email
-    current_user.username = user.username
-    current_user.password = get_password_hash(user.password)
-
-    try:
-        session.add(current_user)
-        session.commit()
-        session.refresh(current_user)
-
-        return current_user
-    except IntegrityError:
-        raise HTTPException(
-            detail='Username ou email ja existe',
-            status_code=HTTPStatus.CONFLICT,
-        )
-
-
-@app.delete('/users/{user_id}', response_model=Message, tags=['Users'])
-def delete_user(
-    user_id: int,
-    session: Session = Depends(get_session),
-    current_user: User = Depends(get_current_user),
-):
-    if current_user.id != user_id:
-        raise HTTPException(
-            status_code=HTTPStatus.FORBIDDEN, detail='permissões insuficientes'
-        )
-
-    session.delete(current_user)
-    session.commit()
-
-    return {'message': 'Usuario deletado com sucesso'}
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message, tags=['hello'])
+def read_root():
+    return {'message': 'Olá mundo'}
