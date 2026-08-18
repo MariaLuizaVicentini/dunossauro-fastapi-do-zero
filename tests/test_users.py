7532900eae5fdf19acc4c42a72fb652df619afab
malu vicentini <maria@multi360.com.br>
2026-08-18 20:19:43 -0300

    fix(testUsers): agrupa teste ref a usuarios

---
+++ tests/test_users.py
@@ -0,0 +1,105 @@
+from http import HTTPStatus
+
+from dunossauro_fastapi.schemas import UserPublic
+
+
+def test_create_user_sucess(client):
+    payload = {
+        'username': 'malu',
+        'email': 'test@malu.com',
+        'password': 'vodoeprajacu',
+    }
+
+    response = client.post('/users/', json=payload)
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_create_integrity_error(client, user):
+    response = client.post(
+        '/users',
+        json={
+            'username': 'Teste',
+            'email': 'fausto@example.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CONFLICT
+    assert response.json() == {'detail': 'Username ou email ja existe'}
+
+
+def test_read_users_sucess(client, token):
+    response = client.get(
+        '/users',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'id': 1,
+                'username': 'Teste',
+                'email': 'teste@test.com',
+            },
+        ],
+    }
+
+
+def test_read_users_with_users_sucess(client, user, token):
+    user_schema = UserPublic.model_validate(user).model_dump()
+
+    response = client.get('/users', headers={'Authorization': f'Bearer {token}'})
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_update_user_success(client, user, token):
+    payload = {
+        'username': 'malu',
+        'email': 'test@malu.com',
+        'password': 'vodoeprajacu',
+    }
+
+    response = client.put(
+        '/users/1', json=payload, headers={'Authorization': f'Bearer {token}'}
+    )
+
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_update_integrity_error(client, user, token):
+    client.post(
+        '/users',
+        json={
+            'username': 'fausto',
+            'email': 'fausto@example.com',
+            'password': 'secret',
+        },
+    )
+
+    response = client.put(
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+        json={
+            'username': 'fausto',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CONFLICT
+    assert response.json() == {'detail': 'Username ou email ja existe'}
+
+
+def test_delete_user_success(client, user, token):
+    message_success = {'message': 'Usuario deletado com sucesso'}
+
+    response = client.delete(
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == message_success
